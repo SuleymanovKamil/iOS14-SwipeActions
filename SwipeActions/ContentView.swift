2616f91f2abe1d47c5c772920fa44d294ac30a9d
Kamil Suleymanov <krakatoor@mail.ru>
2021-08-26 15:05:19 +0300

    Initial Commit

---
+++ SwipeActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipeActions
+//
+//  Created by Камиль Сулейманов on 26.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipeActions/SwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeActionsApp.swift
+//  SwipeActions
+//
+//  Created by Камиль Сулейманов on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
